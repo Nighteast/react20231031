e5cd7f3c651e8a335cd2f7bf2d577343fa8cab91
Nighteast <jung946244@naver.com>
2023-10-31 14:25:20 +0900

    종료태그 작성연습 완료, 컴포넌트 여러번 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+function MyComponent1() {
+  return (
+    <div style={{ width: "100%" }}>
+      <img
+        style={{ width: "100%" }}
+        src="https://i.imgur.com/Af67nss.png"
+        alt="ghost"
+      />
+      ;
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      <MyComponent1 />
+      <MyComponent1 />
+      {/*
+      <img src="https://i.imgur.com/Af67nss.png" />
+      <img src="https://i.imgur.com/Af67nss.png" />
+      <img src="https://i.imgur.com/Af67nss.png" />
+      */}
     </div>
   );
 }
