71ad76f7b64f696c93f65d7f50aedf24af7cdf8f
Nighteast <jung946244@naver.com>
2023-10-31 12:14:36 +0900

    component 연습, 작성완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+function MyComponent1() {
+    return  (
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+    );
+}
 
+function MyComponent2() {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Est.</li>
+                <li>Necessitatibus.</li>
+                <li>Quasi!</li>
+            </ul>
+        </>
+    );
+}
 function App() {
   return (
     <div>
-
+        <MyComponent1 />
+        {/*
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+        */}
+        <MyComponent2 />
+        {/*
+        <p>Lorem ipsum dolor sit.</p>
+        <ul>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+            <li>Lorem.</li>
+        </ul>
+        */}
     </div>
   );
 }
+
+
+
 
 export default App;
