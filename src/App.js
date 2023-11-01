37d5c356c57acc055a2c21333755f929f83df2b4
Nighteast <jung946244@naver.com>
2023-11-01 11:15:14 +0900

    props destructuring assignment 구조분해할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-
 function App() {
   return (
     <div>
+      {/* MyBox Component 2번 사용해 보기 */}
+      <MyBox
+        text={"Lorem ipsum dolor sit amet."}
+        p={"10px"}
+        m={"10px"}
+        color={"gold"}
+        bg={"black"}
+      />
+      <MyBox
+        text={"호롤로로롤로"}
+        p={"10px"}
+        m={"10px"}
+        color={"red"}
+        bg={"black"}
+      />
+    </div>
+  );
+}
 
+function MyBox({ p, m, color, text, bg }) {
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <p>{text}</p>
     </div>
   );
 }
