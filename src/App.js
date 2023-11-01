6f6f6134cdbb6d1e2618c2f88130a04ad13590f1
Nighteast <jung946244@naver.com>
2023-11-01 14:15:49 +0900

    default export, import

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import {
-  MyBox,
-  MyElem,
-  MyContainer,
-  name,
-  person,
-  address,
-} from "./component/MyBox";
+// default는 중괄호 없이 import한다.
+// '한 파일에 하나'라는 규칙이 있기 때문에 이름은 아무렇게나 지을 수 있다.
+import abcdefg, { city } from "./component/MyElem";
+import MyContainer, { person, country } from "./component/MyBox";
 
 function App() {
-  console.log("name", name);
-  console.log("address", address);
-  console.log("person", person);
   return (
     <>
-      <MyBox />
-      <MyElem />
+      <h1>{city}</h1>
+      <h1>{abcdefg}</h1>
       <MyContainer />
+      <h1>
+        {person.age}, {person.name}
+      </h1>
+      <h1>{country}</h1>
     </>
   );
 }
