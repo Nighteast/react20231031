deb41c6d936b9d0a4ec261eeaaa8c700de2513ff
Nighteast <jung946244@naver.com>
2023-11-01 16:39:45 +0900

    css module을 사용할 때 여러 클래스명을 주는 방법 연습 완료

--- src/component/MyText.js
+++ src/component/MyText.js
@@ -7,6 +7,12 @@ export function MyText() {
       {/* src/style/mystyle.module.css 파일 생성 */}
       {/* MyText.js 에서 mystyle.module.css import하기 */}
       <h1 className={style.error}>Lorem ipsum dolor sit amet.</h1>
+      <h1 className={`${style.error} ${style.special} ${style.coffee}`}>
+        Lorem ipsum dolor sit amet.
+      </h1>
+      <h1 className={[style.error, style.special, style.coffee].join(" ")}>
+        Lorem ipsum dolor sit amet.
+      </h1>
     </div>
   );
 }
