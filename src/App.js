d370c5b84ac4ac2c00bc7633364a66d462ae2279
Nighteast <jung946244@naver.com>
2023-11-01 09:46:10 +0900

    props 작성, 활용연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+function MyHeader(props) {
+  return (
+    <>
+      <h1 style={{ color: props.color, backgroundColor: props.bg }}>
+        {props.text}
+      </h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyHeader color={"white"} bg={"black"} text={"내가 만든 제목"} />
+      <MyHeader color={"orange"} bg={"skyblue"} text={"다시 만든 제목"} />
+      {/*
+      <div style="color: white, background-color: black">
+        <h1>내가 만든 제목</h1>
+      </div>
+      <div style="color: orange, background-color: skyblue">
+        <h1>다시 만든 제목</h1>
+      </div>
+      */}
     </div>
   );
 }
