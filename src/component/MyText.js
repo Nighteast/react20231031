f0abe0a2b26bb1da64b1fd0df5e83e5f5c77689e
Nighteast <jung946244@naver.com>
2023-11-01 15:40:01 +0900

    보통의 css 파일 사용하기 연습 완료

---
+++ src/component/MyText.js
@@ -0,0 +1,15 @@
+import "../style/mystyle.css";
+
+export function MyText() {
+  return (
+    <div>
+      {/* red 배경색, white 글자색 */}
+      {/* src/style/mystyle.css 파일 생성 */}
+      {/* MyText.js 에서 mystyle.css import하기 */}
+      <h1 style={{ color: "white", backgroundColor: "red" }}>
+        Lorem ipsum dolor sit amet.
+      </h1>
+      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
