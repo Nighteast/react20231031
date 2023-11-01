5c3db82863eb8ea0ca6e923601f0a8b8fe39e121
Nighteast <jung946244@naver.com>
2023-11-01 11:33:51 +0900

    props 구조분해할당, 기본값 주기 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import * as PropTypes from "prop-types";
+
+function MyComp({
+  m = "5px",
+  p = "5px",
+  color = "red",
+  bg = "green",
+  text = "HALLELUJAH",
+}) {
+  return (
+    <div>
+      <h1 style={{ margin: m, padding: p, color: color, backgroundColor: bg }}>
+        {text}
+      </h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyComp
+        m={"30px"}
+        p={"10px"}
+        color={"gold"}
+        bg={"black"}
+        text={"hello react"}
+      />
+      <MyComp m={"30px"} p={"10px"} color={"gold"} bg={"black"} />
+      <MyComp m={"30px"} bg={"black"} text={"hello react"} />
+      <MyComp p={"10px"} color={"gold"} text={"hello react"} />
     </div>
   );
 }
