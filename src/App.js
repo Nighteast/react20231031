6b852995cfb631cec814ce84147ab2afd3a55d23
Nighteast <jung946244@naver.com>
2023-11-01 10:16:03 +0900

    props 작성, 활용 연습 완료2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-
 function App() {
   return (
     <div>
+      <MyText
+        value={"적절한 값"}
+        style={{
+          color: "yellow",
+          border: "5px solid blue",
+          backgroundColor: "black",
+        }}
+      />
+      <MyText
+        value={"적절한 값2"}
+        style={{
+          color: "red",
+          border: "5px solid gold",
+          backgroundColor: "black",
+        }}
+      />
+    </div>
+  );
+}
 
+function MyText(props) {
+  return (
+    <div style={props.style}>
+      <p>{props.value}</p>
     </div>
   );
 }
