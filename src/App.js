0b8ae6c7179e1beff7c249a6536d5d3dde1cf784
Nighteast <jung946244@naver.com>
2023-11-01 14:35:40 +0900

    여러 파일에서 중복된 named export를 import하는 방법

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-// default는 중괄호 없이 import한다.
-// '한 파일에 하나'라는 규칙이 있기 때문에 이름은 아무렇게나 지을 수 있다.
-import abcdefg, { city } from "./component/MyElem";
-import MyContainer, { person, country } from "./component/MyBox";
+import MyBox, { address as yourAddress } from "./component/MyBox";
+import MyElem, { address } from "./component/MyElem";
+
+// as로 named export 값을 import할 때 별칭을 줄 수 있음
 
 function App() {
   return (
     <>
-      <h1>{city}</h1>
-      <h1>{abcdefg}</h1>
-      <MyContainer />
-      <h1>
-        {person.age}, {person.name}
-      </h1>
-      <h1>{country}</h1>
+      <h1>{address}</h1>
+      <h1>{yourAddress}</h1>
+      <MyElem />
+      <MyBox />
     </>
   );
 }
