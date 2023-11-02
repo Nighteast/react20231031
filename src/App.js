100ece57cf4ef23f782546b1d24db4b754b5314d
Nighteast <jung946244@naver.com>
2023-11-02 09:34:28 +0900

    필요없는 파일들 삭제함

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-// style 적용 방법
-// 일반적인 css 파일 사용
-// css module 사용
-// chakraui css library (공식 문서에서 많이 언급)
-
-import { MyComp } from "./component/MyComp";
-import { MyBox } from "./component/MyBox";
-import { MyContainer } from "./component/MyContainer";
-
 function App() {
-  return (
-    <>
-      <MyComp />
-      <MyBox />
-      <MyContainer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
