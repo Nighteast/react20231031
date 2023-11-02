a0004550f072258d09ec722e225d9e6a24577367
Nighteast <jung946244@naver.com>
2023-11-02 14:34:40 +0900

    Button 사용

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import { Button, useDisclosure, useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import {ArrowRightIcon, EmailIcon} from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
-
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 완료",
-            description: "게시물이 저장되었습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Lorem.</Button>
+      <Button colorScheme="yellow">Odio.</Button>
+      <Button colorScheme="orange">Accusantium?</Button>
+      <Button colorScheme="teal" size={"sm"}>
+        Quia!
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 실패",
-            description: "게시물이 저장되지 않았습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-            status: "error",
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="telegram" size={"lg"}>
+        Soluta!
       </Button>
+      <Button colorScheme="orange" variant={"ghost"}>
+        Doloribus.
+      </Button>
+      <Button variant={"link"}>Voluptatibus!</Button>
+      <Button leftIcon={<EmailIcon/>}>Eum.</Button>
+      <Button rightIcon={<ArrowRightIcon />}>Quod.</Button>
+      <Button isLoading>Dicta.</Button>
+      <Button isLoading loadingText={"전송중"}>Iste.</Button>
+      <Button>Aperiam?</Button>
+      <Button>Dolore!</Button>
+      <Button>Neque!</Button>
+      <Button>Porro.</Button>
+      <Button>Perferendis.</Button>
+      <Button>Voluptate.</Button>
+      <Button>Esse.</Button>
+      <Button>Dolorem.</Button>
+      <Button>Ea.</Button>
+      <Button>Qui.</Button>
+      <Button>Temporibus.</Button>
+      <Button>Pariatur.</Button>
+      <Button>Rem!</Button>
+      <Button>Veniam.</Button>
+      <Button>Doloribus!</Button>
+      <Button>Distinctio?</Button>
+      <Button>Nemo.</Button>
+      <Button>Nesciunt.</Button>
+      <Button>Perferendis.</Button>
     </>
   );
 }
