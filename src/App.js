809300bd447df7afe90c2b7976d5912a07aae9b7
Nighteast <jung946244@naver.com>
2023-11-02 15:15:32 +0900

    Select 사용

--- src/App.js
+++ src/App.js
@@ -1,60 +1,21 @@
-import {
-  Box,
-  Center,
-  Checkbox,
-  CheckboxGroup,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-} from "@chakra-ui/react";
+import React from "react";
+import { Box, FormControl, Select } from "@chakra-ui/react";
 
-function App() {
+function App(props) {
   return (
-    <>
-      <Center>
-        <Box maxW={"200px"}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-            <FormHelperText>띄어쓰기 없이 입력하세요.</FormHelperText>
-          </FormControl>
-        </Box>
-      </Center>
-
-      <Center>
-        <Box maxW={"200px"}>
-          <FormControl>
-            <FormLabel></FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">Lorem.</Radio>
-                <Radio value="value2">Fugiat!</Radio>
-                <Radio value="value3">Exercitationem.</Radio>
-                <Radio value="value4">Suscipit!</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-
-      <Center>
-        <Box>
-          <FormControl>
-            <CheckboxGroup>
-              <Flex gap={5}>
-                <Checkbox colorScheme="orange">Lorem.</Checkbox>
-                <Checkbox colorScheme="green">Totam.</Checkbox>
-                <Checkbox colorScheme="purple">Est!</Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </FormControl>
-        </Box>
-      </Center>
-    </>
+    <div>
+      <Box w={"480px"}>
+        <FormControl>
+          <Select placeholder="선택하세요." multiple h={"150px"}>
+            <option value="value1">Lorem.</option>
+            <option value="value2">Lorem.</option>
+            <option value="value3">Lorem.</option>
+            <option value="value4">Lorem.</option>
+            <option value="value5">Lorem.</option>
+          </Select>
+        </FormControl>
+      </Box>
+    </div>
   );
 }
 
