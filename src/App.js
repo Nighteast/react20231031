7aea59a8a5b5e3dbb178c500767be559ce150636
Nighteast <jung946244@naver.com>
2023-11-02 12:37:37 +0900

    Center, Square, Circle 사용

--- src/App.js
+++ src/App.js
@@ -1,71 +1,19 @@
-import {
-  Box,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Flex,
-  Heading,
-  Button,
-  SimpleGrid,
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-} from "@chakra-ui/react";
+import { Box, Center, Circle, Square } from "@chakra-ui/react";
+import { AtSignIcon, ChatIcon, PhoneIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <>
-      <SimpleGrid columns={1} gap={5}>
-        <Box>
-          {/*Box>Table[variant=striped]>Thead>Tr>Th*4>lorem1^^^Tbody>Tr*5>Td*4>lorem1*/}
-          <Table variant="striped" colorScheme="red">
-            <Thead>
-              <Tr>
-                <Th>Lorem.</Th>
-                <Th>Similique!</Th>
-                <Th>Ipsa!</Th>
-                <Th>Eligendi.</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Velit!</Td>
-                <Td>Consequatur.</Td>
-                <Td>Autem.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Minus!</Td>
-                <Td>Aliquid.</Td>
-                <Td>Aliquid.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Id?</Td>
-                <Td>Distinctio.</Td>
-                <Td>Fugit.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Placeat?</Td>
-                <Td>Nisi.</Td>
-                <Td>Sapiente.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem.</Td>
-                <Td>Libero!</Td>
-                <Td>Iure.</Td>
-                <Td>Suscipit.</Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </Box>
-      </SimpleGrid>
+      <Center bg={"red.200"} h={"200px"}>
+        <Square bg={"blue.300"} w={"100px"} h={"100px"}>
+          <AtSignIcon />
+        </Square>
+      </Center>
+      <Center bg={"red.400"} h={"200px"}>
+        <Circle bg={"blue.200"} w={"100px"} h={"100px"}>
+          <ChatIcon />
+        </Circle>
+      </Center>
     </>
   );
 }
