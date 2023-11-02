8e45c1e808a949eda8ccbe8456f6e38e99e3606b
Nighteast <jung946244@naver.com>
2023-11-02 10:18:21 +0900

    ChakraUi Box 컴포넌트, color 프로미터 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { Box } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box bgColor={"tomato"}>Lorem ipsum dolor.</Box>
+      <Box bgColor={"black"} color={"aqua"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box bgColor={"violet"}>Animi iusto, quos.</Box>
+      <Box bgColor={"gray.100"}>Commodi, facere, neque?</Box>
+      <Box bgColor={"darkred"} color={"linkedin.200"}>
+        Aliquam, aperiam maiores!
+      </Box>
+      <Box bgColor={"blue.900"} color={"linkedin.200"}>
+        Aliquam, ex, omnis.
+      </Box>
+      <Box bgColor={"green.900"} color={"linkedin.200"}>
+        Dolorum quisquam, reiciendis?
+      </Box>
+      <Box bgColor={"purple"} color={"linkedin.200"}>
+        Atque exercitationem, nam!
+      </Box>
+      <Box bgColor={"aqua"}>Nam, optio, voluptate!</Box>
+      <Box bgColor={"yellow.600"} color={"gray.200"}>
+        Id, tempora tenetur?
+      </Box>
+      <Box bgColor={"facebook.800"} color={"linkedin.200"}>
+        Assumenda quisquam, voluptates?
+      </Box>
+    </>
+  );
 }
 
 export default App;
