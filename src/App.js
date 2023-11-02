ecd32eb4846124e55c3b9997ec9bd31ecfd405f0
Nighteast <jung946244@naver.com>
2023-11-02 14:11:33 +0900

    Modal 사용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import { Box, Center, Circle, Square } from "@chakra-ui/react";
-import { AtSignIcon, ChatIcon, PhoneIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
-      <Center bg={"red.200"} h={"200px"}>
-        <Square bg={"blue.300"} w={"100px"} h={"100px"}>
-          <AtSignIcon />
-        </Square>
-      </Center>
-      <Center bg={"red.400"} h={"200px"}>
-        <Circle bg={"blue.200"} w={"100px"} h={"100px"}>
-          <ChatIcon />
-        </Circle>
-      </Center>
+      <Button onClick={onOpen}>모달 열기</Button>
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>모달의 제목</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+            accusamus accusantium aliquam animi aspernatur at atque distinctio
+            dolorum ducimus eius fugiat hic maiores, nobis quae reprehenderit
+            suscipit totam! Asperiores, molestiae.
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              닫기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
