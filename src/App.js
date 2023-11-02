f42fa9413d8488411a4418ac47c868bd48bf4082
Nighteast <jung946244@naver.com>
2023-11-02 16:31:28 +0900

    bootstrap navbar 만들어 보기

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
 import React from "react";
-import { Box, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 function App(props) {
   return (
-    <div>
-      <Box w={"480px"}>
-        <Image src="https://i.imgur.com/Z4UsRCx.png" />
-      </Box>
-      <Box>
-        <Image boxSize={"100px"} src="https://i.imgur.com/Z4UsRCx.png" />
-      </Box>
-      <Box>
-        <Image boxSize={"200px"} src="https://i.imgur.com/Z4UsRCx.png" />
-      </Box>
-      <Box>
-        <Image
-          borderRadius={"full"}
-          boxSize={"100px"}
-          src="https://i.imgur.com/Z4UsRCx.png"
-        />
-      </Box>
-      <Box>
-        <Image
-          fallbackSrc="https://via.placeholder.com/150"
-          boxSize={"100px"}
-          src="abcde.jpg"
-        />
-      </Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-      <Box></Box>
-    </div>
+    <Flex
+      justifyContent={"space-between"}
+      align={"center"}
+      py={"8px"}
+      my={"10px"}
+      bg={"gray.100"}
+      px={{ base: "0px", md: "70px", lg: "120px", xl: "180px" }}
+    >
+      <Flex gap={"7px"} align={"center"}>
+        <Text as={"b"}>Navbar</Text>
+        <Button variant={"ghost"}>Home</Button>
+        <Button variant={"ghost"}>Link</Button>
+        <Menu>
+          <MenuButton
+            variant={"ghost"}
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+          >
+            Dropdown
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Download</MenuItem>
+            <MenuItem>Create a Copy</MenuItem>
+            <MenuItem>Mark as Draft</MenuItem>
+            <MenuItem>Delete</MenuItem>
+            <MenuItem>Attend a Workshop</MenuItem>
+          </MenuList>
+        </Menu>
+        <Button variant={"ghost"} isDisabled={true}>
+          Disable
+        </Button>
+      </Flex>
+      <Flex gap={"7px"} align={"center"}>
+        <FormControl>
+          <Input size={"sm"} bg={"white"} />
+        </FormControl>
+        <Button size={"sm"} variant={"outline"} colorScheme="green">
+          Search
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
 
