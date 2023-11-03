e5b85c89cd09a64f00b4340dcf23b675506aeea9
Nighteast <jung946244@naver.com>
2023-11-03 09:35:48 +0900

    children 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { Button, Link } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <div>
+      <a href={to}>{children}</a>
+
+      <Button as="a" href={to}>
+        {children}
+      </Button>
+    </div>
+  );
+}
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyLink to={"https://www.naver.com"}>naver로</MyLink>
+      <MyLink to={"https://www.daum.net"}>daum으로</MyLink>
+      {/* 
+    <a href="https://www.naver.com">naver로</a>
+    <a href="https://www.daum.net">daum으로</a>
+     */}
+    </>
+  );
 }
 
 export default App;
