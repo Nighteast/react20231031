9058f1cf6b72dce3c9d3a845ff8bf20f28953f4d
Nighteast <jung946244@naver.com>
2023-11-03 12:33:15 +0900

    event.preventDefault() : 기본 기능 취소 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-
 function App() {
-  return <></>;
+  function handleClick(e) {
+    // a 태그 클릭시 naver로 이동
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
 
 export default App;
