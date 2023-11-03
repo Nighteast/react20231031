46c606be5cb2033f27125292a262f7768452e0c4
Nighteast <jung946244@naver.com>
2023-11-03 09:17:59 +0900

    children prop

--- src/App.js
+++ src/App.js
@@ -1,61 +1,20 @@
-import React from "react";
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  Input,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-} from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+function MyComp({ text, address, children }) {
+  return (
+    <>
+      <p>{text}</p>
+      <p>{address}</p>
+      <p>{children}</p>
+    </>
+  );
+}
 
 function App(props) {
   return (
-    <Flex
-      justifyContent={"space-between"}
-      align={"center"}
-      py={"8px"}
-      my={"10px"}
-      bg={"gray.100"}
-      px={{ base: "0px", md: "70px", lg: "120px", xl: "180px" }}
-    >
-      <Flex gap={"7px"} align={"center"}>
-        <Text as={"b"}>Navbar</Text>
-        <Button variant={"ghost"}>Home</Button>
-        <Button variant={"ghost"}>Link</Button>
-        <Menu>
-          <MenuButton
-            variant={"ghost"}
-            as={Button}
-            rightIcon={<ChevronDownIcon />}
-          >
-            Dropdown
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-        <Button variant={"ghost"} isDisabled={true}>
-          Disable
-        </Button>
-      </Flex>
-      <Flex gap={"7px"} align={"center"}>
-        <FormControl>
-          <Input size={"sm"} bg={"white"} />
-        </FormControl>
-        <Button size={"sm"} variant={"outline"} colorScheme="green">
-          Search
-        </Button>
-      </Flex>
-    </Flex>
+    <div>
+      <MyComp text={"hello"} address={"seoul"}>
+        Some content, 다른 컴포넌트가 있을 수도 있음.
+      </MyComp>
+    </div>
   );
 }
 
