01df4bbf135945b1cb4a6de01387c1dd3b294596
Nighteast <jung946244@naver.com>
2023-11-06 14:16:51 +0900

    배열인 상태를 복사해서 상태 변경 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import { useState } from "react";
+import { Box, Button, List, ListItem, OrderedList } from "@chakra-ui/react";
 
 function App() {
-  return <></>;
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick(item) {
+    console.log(item);
+    // items의 상태를 item을 추가하도록 변경
+    // const newItems = [...items];
+    // newItems.push(item);
+
+    // const newItems = [...items, item];
+    // setItems(newItems);
+
+    setItems([...items, item]);
+
+    console.log(items);
+  }
+
+  return (
+    <>
+      <Button onClick={() => handleButtonClick("커피")}>커피</Button>
+      <Button onClick={() => handleButtonClick("케익")}>케익</Button>
+      <Button onClick={() => handleButtonClick("아이스크림")}>
+        아이스크림
+      </Button>
+      <Button onClick={() => handleButtonClick("크로아상")}>크로아상</Button>
+      <Button onClick={() => handleButtonClick("단팥빵")}>단팥빵</Button>
+
+      <Box>
+        <OrderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </OrderedList>
+      </Box>
+    </>
+  );
 }
 
 export default App;
