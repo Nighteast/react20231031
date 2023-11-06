c6d002c8ad21d27e57a653b0157eb5da2041256a
Nighteast <jung946244@naver.com>
2023-11-06 10:17:58 +0900

    state 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [text, setText] = useState("hello");
+  const [number, setNumber] = useState(0);
 
-  console.log(text);
-  function handleButtonClick(e) {
-    // text 라는 state 값 변경
-    setText("greeting");
-  }
+  console.log(number);
+
+  const index = number % 3;
+
+  console.log(index);
 
   return (
     <div>
-      <Button onClick={handleButtonClick}>상태 변경!</Button>
-      <p>{text}</p>
+      <Button onClick={() => setNumber(number + 1)}>다음</Button>
+      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
+        <Text>message1</Text>
+      </Box>
+      <Box borderWidth={"3px"} borderColor={index == 1 ? "blue" : "black"}>
+        <Text>message2</Text>
+      </Box>
+      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
+        <Text>message3</Text>
+      </Box>
     </div>
   );
 }
