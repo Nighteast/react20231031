59053d0f338acc59e02f25f6331a2472a77d37c5
Nighteast <jung946244@naver.com>
2023-11-06 09:53:05 +0900

    state 사용 예제 연습 완료2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import { Button } from "@chakra-ui/react";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
 
-function App() {
-  const [number, setNumber] = useState(0);
+function App(props) {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  function handleUserNameChange(e) {
+    setUsername(e.target.value);
+  }
 
-  function handleButtonClick(e) {
-    setNumber(number + e);
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
   }
 
   return (
-    <>
-      <h1>{number}</h1>
-      <Button
-        onClick={() => {
-          handleButtonClick(1);
-        }}
-      >
-        증가
-      </Button>
-      <Button
-        onClick={() => {
-          handleButtonClick(-1);
-        }}
-      >
-        감소
-      </Button>
-    </>
+    <div>
+      <Box>
+        <Input type="text" value={username} onChange={handleUserNameChange} />
+      </Box>
+      <Box>
+        <Input type="text" value={email} onChange={handleEmailChange} />
+      </Box>
+      <Text>
+        <span style={{ color: "aqua", backgroundColor: "black" }}>
+          {username}
+        </span>
+        의 email은
+        <span style={{ color: "aqua", backgroundColor: "black" }}>{email}</span>
+        입니다.
+      </Text>
+
+      <Box>
+        <Input
+          type="text"
+          value={address}
+          onChange={(e) => {
+            handleAddressChange(e.target.value);
+          }}
+        />
+      </Box>
+    </div>
   );
 }
 
