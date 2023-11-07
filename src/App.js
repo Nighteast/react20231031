ebb9aa36067d9f805e7d47427ed13641f1231947
Nighteast <jung946244@naver.com>
2023-11-07 14:43:14 +0900

    다른 파일에 있는 Component 가 Context 사용하기 위해서는 Context 는 export 해야 함. 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
-import React, { createContext, useContext, useState } from "react";
-import { Input, Text } from "@chakra-ui/react";
-
-function MyInput() {
-  const { text, setText } = useContext(TextContext);
-
-  return (
-    <Input
-      value={text}
-      onChange={(e) => {
-        setText(e.target.value);
-      }}
-    />
-  );
-}
-
-function MyText() {
-  const { text } = useContext(TextContext);
-
-  return <Text>{text}</Text>;
-}
+import React, { createContext, useState } from "react";
+import { MyInput } from "./component/MyInput";
+import { MyText } from "./component/MyText";
 
 function App(props) {
   const [text, setText] = useState("초기값");
@@ -32,5 +14,5 @@ function App(props) {
   );
 }
 
-const TextContext = createContext(null);
+export const TextContext = createContext(null);
 export default App;
